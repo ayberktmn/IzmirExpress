7ed229be2acabc91df3a057d24cd6aa670e184f6
ayberktmn <ayberktemin@gmail.com>
2024-01-03 16:49:00 +0300

    museums added

--- app/src/main/java/com/ayberk/IzmirExpress/MainActivity.kt
+++ app/src/main/java/com/ayberk/IzmirExpress/MainActivity.kt
@@ -3,15 +3,19 @@ package com.ayberk.IzmirExpress
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.ayberk.IzmirExpress.model.Museums
 import com.ayberk.IzmirExpress.navigation.Navigation
 import com.ayberk.IzmirExpress.ui.theme.IzmirIlkYardimTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             IzmirIlkYardimTheme {
               Navigation()
+
             }
         }
     }
--- app/src/main/java/com/ayberk/IzmirExpress/Muzeler.kt
+++ app/src/main/java/com/ayberk/IzmirExpress/Muzeler.kt
@@ -1,9 +1,152 @@
 package com.ayberk.IzmirExpress
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import com.ayberk.IzmirExpress.model.Onemliyer
+import com.ayberk.IzmirExpress.viewmodel.DataViewModel
+import com.ayberk.izmirilkyardim.R
+import dagger.hilt.android.AndroidEntryPoint
+
 
 @Composable
-fun Muzeler(navHostController:NavHostController) {
+fun Muzeler(
+    navHostController: NavHostController,
+    museumViewModel: DataViewModel = hiltViewModel()
+) {
+    MuseumsList(navHostController = navHostController)
+}
+
+@Composable
+fun MuseumsList(
+    navHostController: NavHostController,
+    viewModel: DataViewModel = hiltViewModel()
+) {
+    val museumsList by remember { viewModel.museumsList }
+    val errorMessage by remember { viewModel.errorMessage }
+    val isLoading by remember { viewModel.isLoading }
+
+    MuseumsItemGrid(museums = museumsList,navHostController = navHostController)
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if(isLoading) {
+            CircularProgressIndicator(color = Color.Blue)
+        }
+        if(errorMessage.isNotEmpty()) {
+            RetryView(error = errorMessage) {
+                viewModel.loadMuseums()
+            }
+        }
+
+    }
 
 }
+@Composable
+fun MuseumsItemGrid(navHostController: NavHostController, museums: List<Onemliyer>) {
+    LazyVerticalGrid(GridCells.Fixed(1)) {
+        items(museums) { item ->
+            MuseumsRow(navHostController = navHostController, museums = item)
+        }
+    }
+}
+
+@Composable
+fun MuseumsRow(navHostController: NavHostController, museums: Onemliyer) {
+    Card (
+        modifier = Modifier
+            .background(Color.White)
+            .padding(8.dp),
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(Color.Red)
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.clocktower),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(60.dp)
+                    .padding(end = 8.dp), // İsteğe bağlı: İmage ile Text arasında bir boşluk ekler
+            )
+
+            Text(
+                text = museums.ADI,
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier
+                    .align(Alignment.Top)
+
+            )
+        }
+    }
+}
+
+@Composable
+fun RetryView(
+    error: String,
+    onRetry: () -> Unit
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(error, color = Color.Red, fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            onClick = { onRetry() },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Retry")
+        }
+    }
+}
+

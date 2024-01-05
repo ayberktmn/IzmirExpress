12af623caa4645eb3f6e2b4d88474a97eb4ad425
ayberktmn <ayberktemin@gmail.com>
2024-01-05 15:27:33 +0300

    pharmacy screen done

--- app/src/main/java/com/ayberk/IzmirExpress/Pharmacy.kt
+++ app/src/main/java/com/ayberk/IzmirExpress/Pharmacy.kt
@@ -1,5 +1,7 @@
 package com.ayberk.IzmirExpress
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -7,25 +9,36 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -36,12 +49,16 @@ import com.ayberk.IzmirExpress.model.PharmacyItem
 import com.ayberk.IzmirExpress.ui.theme.blue
 import com.ayberk.IzmirExpress.viewmodel.DataViewModel
 import com.ayberk.izmirilkyardim.R
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun Pharmacy(navHostController: NavHostController) {
     PharmacyList()
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun PharmacyList(viewModel: DataViewModel = hiltViewModel()) {
     val pharmacyList by remember { viewModel.pharmacyList }
@@ -67,6 +84,7 @@ fun PharmacyList(viewModel: DataViewModel = hiltViewModel()) {
 
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun PharmacyItemGrid(pharmacy: List<PharmacyItem>) {
     LazyVerticalGrid(GridCells.Fixed(2)){
@@ -76,11 +94,28 @@ fun PharmacyItemGrid(pharmacy: List<PharmacyItem>) {
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun PharmacyItem(pharmacy: PharmacyItem) {
+    val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+    val outputFormatter = DateTimeFormatter.ofPattern("dd/MM/yy")
+
+    val parsedDate = LocalDateTime.parse(pharmacy.Tarih, inputFormatter)
+    val formattedDate = parsedDate.format(outputFormatter)
+
+    val customFontFamily = FontFamily(
+        Font(R.font.inter)
+    )
+
+    val bolgeText = pharmacy.Bolge
+    val detailsbolgeText = bolgeText.substringBefore('-').trim()
+
+    var expanded by remember { mutableStateOf(false) }
+
     Card(
         modifier = Modifier
-            .size(100.dp, 100.dp)
+            .fillMaxWidth()
+
             .padding(8.dp),
         shape = MaterialTheme.shapes.medium,
         colors = CardDefaults.cardColors(Color.Red)
@@ -92,8 +127,8 @@ fun PharmacyItem(pharmacy: PharmacyItem) {
         ) {
             Spacer(modifier = Modifier.height(16.dp))
             Image(
-                painter = painterResource(id = R.drawable.pharmacy),
-                contentDescription ="pharmacy",
+                painter = painterResource(id = R.drawable.pharmacydetails),
+                contentDescription = "pharmacy",
                 modifier = Modifier
                     .size(80.dp)
                     .padding(start = 16.dp)
@@ -102,11 +137,72 @@ fun PharmacyItem(pharmacy: PharmacyItem) {
                 text = pharmacy.Adi,
                 textAlign = TextAlign.Center,
                 color = Color.White,
-                fontWeight = FontWeight.Bold,
+                fontFamily = customFontFamily,
                 style = MaterialTheme.typography.labelLarge,
                 modifier = Modifier
                     .padding(start = 4.dp)
             )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = detailsbolgeText,
+                textAlign = TextAlign.Start,
+                color = Color.Black,
+                style = MaterialTheme.typography.labelMedium,
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = formattedDate,
+                textAlign = TextAlign.End,
+                color = Color.White,
+                style = MaterialTheme.typography.labelMedium
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+                if (expanded) {
+                    Text(
+                        text = pharmacy.Adres,
+                        style = MaterialTheme.typography.labelLarge,
+                        color = Color.White,
+                        modifier = Modifier
+                            .padding(start = 8.dp)
+                            .padding(top = 8.dp)
+                    )
+                    IconButton(
+                        onClick = { expanded = false },
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.KeyboardArrowUp,
+                            contentDescription = null,
+                            tint = Color.White // Beyaz renkli olacak
+                        )
+                    }
+                } else {
+                    IconButton(
+                        onClick = { expanded = true },
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally) // İkonu dikeyde ortalamak için
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.KeyboardArrowDown,
+                            contentDescription = null,
+                            tint = Color.White // Beyaz renkli olacak
+                   )
+                }
+            }
         }
     }
 }

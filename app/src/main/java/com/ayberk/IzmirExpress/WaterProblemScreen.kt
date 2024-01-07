71a2ef11512b92e63116cfe9b289270d5cfe03e7
ayberktmn <ayberktemin@gmail.com>
2024-01-07 15:26:08 +0300

    waterproblemdetails added

--- app/src/main/java/com/ayberk/IzmirExpress/WaterProblemScreen.kt
+++ app/src/main/java/com/ayberk/IzmirExpress/WaterProblemScreen.kt
@@ -3,6 +3,7 @@ package com.ayberk.IzmirExpress
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,10 +19,13 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -36,9 +40,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -109,10 +115,19 @@ fun WaterProblemItemList(navHostController: NavHostController, waterProblemItem:
     date = dateTime.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
     time = dateTime.format(DateTimeFormatter.ofPattern("HH:mm"))
 
+    var isProfileImageClicked by remember { mutableStateOf(false) }
+
+    val customFontFamily = FontFamily(
+        Font(R.font.inter)
+    )
+
     Card(
         modifier = Modifier
             .fillMaxHeight()
             .padding(8.dp)
+            .clickable {
+                isProfileImageClicked = true
+            }
             .shadow(8.dp, shape = MaterialTheme.shapes.medium),
         shape = MaterialTheme.shapes.medium,
         colors = CardDefaults.cardColors(white)
@@ -124,6 +139,112 @@ fun WaterProblemItemList(navHostController: NavHostController, waterProblemItem:
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         )  {
+            if (isProfileImageClicked) {
+                AlertDialog(
+                    onDismissRequest = {
+                        isProfileImageClicked = false
+                    },
+                    confirmButton = {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.nowater),
+                                contentDescription = "nowater",
+                                modifier = Modifier
+                                    .size(100.dp)
+                            )
+                            Text(
+                                text = waterProblemItem.Tip,
+                                textAlign = TextAlign.Center,
+                                color = blue,
+                                fontFamily = customFontFamily,
+                                modifier = Modifier
+                                    .padding(start = 8.dp)
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            IconButton(onClick = { /* Handle back button click */ }) {
+                                Icon(
+                                    imageVector = Icons.Default.DateRange,
+                                    contentDescription = null,
+                                )
+                            }
+                            Text(
+                                text = date,
+                                modifier = Modifier
+                                    .padding(start = 2.dp)
+                                    .align(Alignment.CenterVertically)
+                            )
+                        }
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            IconButton(onClick = { /* Handle back button click */ }) {
+                                Icon(
+                                    imageVector  = Icons.Default.Home,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(20.dp)
+                                )
+                            }
+                            Text(
+                                text = waterProblemItem.Mahalleler,
+                                modifier = Modifier
+                                    .padding(start = 2.dp)
+                                    .align(Alignment.CenterVertically)
+                            )
+                        }
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            IconButton(onClick = { /* Handle back button click */ }) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.clock),
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(20.dp)
+                                )
+                            }
+                            Text(
+                                text = time,
+                                modifier = Modifier
+                                    .padding(start = 2.dp)
+                                    .align(Alignment.CenterVertically)
+                            )
+                        }
+                        Text(
+                            text = waterProblemItem.Aciklama,
+                            modifier = Modifier
+                                .padding(start = 2.dp)
+                                .align(Alignment.CenterHorizontally),
+                            textAlign = TextAlign.Center,
+                            color = Color.Black
+
+                        )
+                        Text(
+                            text = waterProblemItem.IlceAdi,
+                            modifier = Modifier
+                                .padding(start = 2.dp),
+                            color = Color.Blue
+                        )
+                    },
+                    containerColor = Color.White,
+                )
+            }
             Image(
                 painter = painterResource(id = R.drawable.nowater),
                 contentDescription = "nowater",

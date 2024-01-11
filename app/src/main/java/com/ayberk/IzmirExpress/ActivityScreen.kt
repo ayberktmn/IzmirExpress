7046b5b16554055f451ebae89213421e346f532c
ayberktmn <ayberktemin@gmail.com>
2024-01-11 13:45:32 +0300

    actvitiysScreen alertdialog added

--- app/src/main/java/com/ayberk/IzmirExpress/ActivityScreen.kt
+++ app/src/main/java/com/ayberk/IzmirExpress/ActivityScreen.kt
@@ -1,11 +1,13 @@
 package com.ayberk.IzmirExpress
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,8 +16,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -100,10 +106,16 @@ fun ActivitysItem(activitys:ActivitysItem) {
     date = dateTime.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
     time = dateTime.format(DateTimeFormatter.ofPattern("HH:mm"))
 
+    var isClickedDetails by remember { mutableStateOf(false) }
+    val ucrestsizmi = activitys.UcretsizMi
+
     Card (
         modifier = Modifier
             .size(305.dp)
             .padding(8.dp)
+            .clickable {
+                isClickedDetails = true
+            }
             .shadow(8.dp, shape = MaterialTheme.shapes.medium),
         shape = MaterialTheme.shapes.medium,
         colors = CardDefaults.cardColors(Color.Black),
@@ -174,6 +186,168 @@ fun ActivitysItem(activitys:ActivitysItem) {
                     color = Color.White
                 )
             }
+        }
+    }
+    if (isClickedDetails) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            AlertDialog(
+                onDismissRequest = { isClickedDetails = false },
+                confirmButton = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                            .clickable {
+                                isClickedDetails = true
+                            }
+                    ) {
+                        Image(
+                            painter = rememberImagePainter(data = activitys.Resim),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .padding(top = 8.dp)
+                                .size(150.dp),
+                            contentScale = ContentScale.FillHeight
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = activitys.Adi,
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .padding(top = 8.dp)
+                        )
+                        Text(
+                            text = activitys.KisaAciklama,
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .padding(top = 8.dp),
+                            maxLines = 10
+                        )
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 8.dp)
+                                .padding(horizontal = 16.dp) // Genel yatay iç açıklık
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                IconButton(onClick = { }) {
+                                    Icon(
+                                        imageVector = Icons.Default.DateRange,
+                                        contentDescription = null,
+                                        tint = Color.White
+                                    )
+                                }
+                                Text(
+                                    text = date,
+                                    modifier = Modifier
+                                        .padding(start = 2.dp, end = 2.dp)
+                                        .align(Alignment.CenterVertically),
+                                    color = Color.White
+                                )
+                            }
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                IconButton(onClick = { }) {
+                                    Icon(
+                                        imageVector = Icons.Default.LocationOn,
+                                        contentDescription = null,
+                                        tint = Color.White
+                                    )
+                                }
+                                Text(
+                                    text = activitys.EtkinlikMerkezi,
+                                    modifier = Modifier
+                                        .padding(start = 2.dp, end = 2.dp)
+                                        .align(Alignment.CenterVertically),
+                                    color = Color.White
+                                )
+                            }
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                IconButton(onClick = { }) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.clock),
+                                        contentDescription = null,
+                                        modifier = Modifier
+                                            .size(20.dp),
+                                        tint = Color.White
+                                    )
+                                }
+                                Text(
+                                    text = time,
+                                    modifier = Modifier
+                                        .padding(start = 2.dp)
+                                        .align(Alignment.CenterVertically),
+                                    color = Color.White
+                                )
+                            }
+                            if (ucrestsizmi) {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Center
+                                ) {
+                                    IconButton(onClick = { }) {
+                                        Icon(
+                                            painter = painterResource(id = R.drawable.free),
+                                            contentDescription = null,
+                                            tint = Color.White
+                                        )
+                                    }
+                                    Text(
+                                        text = "Ücretsiz",
+                                        modifier = Modifier
+                                            .padding(start = 2.dp, end = 2.dp)
+                                            .align(Alignment.CenterVertically),
+                                        color = Color.White
+                                    )
+                                }
+                            } else {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Center
+                                ) {
+                                    IconButton(onClick = { }) {
+                                        Icon(
+                                            painter = painterResource(id = R.drawable.ticket),
+                                            contentDescription = null,
+                                            tint = Color.White
+                                        )
+                                    }
+                                    Text(
+                                        text = "Ücretli",
+                                        modifier = Modifier
+                                            .padding(start = 4.dp, end = 2.dp)
+                                            .align(Alignment.CenterVertically),
+                                        color = Color.White
+                                    )
+                                }
+                            }
+                        }
+                    }
+                },
+                containerColor = Color(0xFF1944E0)
+            )
         }
     }
 }
